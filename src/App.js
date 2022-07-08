044c5ff2a2a2173c6fcc541eec71a11804bab51e
Akil Marshall <akilmarshall@gmail.com>
2022-07-07 17:05:24 -0700

    setup gh pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react'
+import { 
+    Heading,
+    Center,
+    Container,
+    Text,
+    VStack,
+    Button,
+    Image,
+} from '@chakra-ui/react'
+import { staticSpriteURL } from './Sprites'
 
 
 function App() {
-  return (
-  <>
-      <h1>Nuzlike</h1>
-  </>
-  );
+    const pokemon = 1 + Math.floor((Math.random() * 386))
+    return (
+    <>
+      <Center>
+          <Heading as='h2' size='3xl'>Nuzlike</Heading>
+      </Center>
+      <Container centerContent>
+        <Text>Your favorite Nuzlocke experience without any of the awful grinding.</Text>
+      </Container>
+      <Center>
+        <Image src={staticSpriteURL(pokemon)} />
+      </Center>
+      <VStack>
+        <Button>Start</Button>
+        <Button>Settings</Button>
+        <Button>About</Button>
+      </VStack>
+    </>
+    );
 }
 
 export default App;
